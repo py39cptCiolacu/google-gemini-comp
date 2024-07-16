2c7b073e92d0caf3d4b3eb47edccf1fe2090fd48
DanielCosmin <dciolacu87@yahoo.com>
2024-07-17 00:07:18 +0300

    issue 3 - made an map with folium and not google api

--- website/views.py
+++ website/views.py
@@ -1,9 +1,40 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, jsonify
+import folium
 
 views = Blueprint("views", __name__)
 
-@views.route('/', methods=["GET", "POST"])
-def home():
+points = []
 
-    return render_template("home.html")
+@views.route('/')
+def home() -> str:
+
+    start_coords = (0, 0)
+    folium_map= folium.Map(location=start_coords, zoom_start=2)
+
+    return render_template("home.html", foium_map = folium_map._repr_html_())
+
+
+@views.route("/get_coordinates", methods=["POST"])
+def get_coordinates():
+
+    global points
+    data = request.get_json()
+    lat = data.get("lat")
+    lng = data.get("lng")
+    points.append((lat, lng))
+
+    if len(points) > 4:
+        points = points[-4:]
+
+    response = {
+        'lat': lat,
+        'lng': lng,
+        'points': points if len(points) == 4 else []
+    }
+
+    return jsonify(response)
+
+
+
+
 
