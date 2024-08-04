2111de22e4d72f161b8b6fe2d771d716943f784f
DanielCosmin <dciolacu87@yahoo.com>
2024-08-04 14:18:10 +0300

    user_profile buttons

--- ui/src/pages/UserProfile/UserProfile.js
+++ ui/src/pages/UserProfile/UserProfile.js
@@ -73,10 +73,10 @@ function UserProfile() {
                   <div className="rounded-top text-white d-flex flex-row" style={{ backgroundColor: '#000', height: '200px' }}>
                     <div className="ms-4 mt-5 d-flex flex-column" style={{ width: '150px' }}>
                       <MDBCardImage src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-profiles/avatar-1.webp"
-                        alt="Generic placeholder image" className="mt-4 mb-2 img-thumbnail" fluid style={{ width: '150px', zIndex: '1' }} />
-                      <MDBBtn outline color="dark" style={{ height: '36px', overflow: 'visible' }}>
+                        alt="Generic placeholder image" className="mt-4 mb-2 img-thumbnail" fluid style={{ width: '200px', zIndex: '1' }} />
+                      {/* <MDBBtn className="d-block">
                         Edit profile
-                      </MDBBtn>
+                      </MDBBtn> */}
                     </div>
                     <div className="ms-3" style={{ marginTop: '130px' }}>
                       <MDBTypography tag="h5"></MDBTypography>
@@ -86,11 +86,11 @@ function UserProfile() {
                   <div className="p-4 text-black" style={{ backgroundColor: '#f8f9fa' }}>
                     <div className="d-flex justify-content-end text-center py-1">
                       <div>
-                        <MDBCardText className="mb-1 h5">253</MDBCardText>
+                        <MDBCardText className="mb-1 h3">8</MDBCardText>
                         <MDBCardText className="small text-muted mb-0">Lands</MDBCardText>
                       </div>
                       <div className="px-3">
-                        <MDBCardText className="mb-1 h5">1026</MDBCardText>
+                        <MDBCardText className="mb-1 h3">30</MDBCardText>
                         <MDBCardText className="small text-muted mb-0">Suggestions</MDBCardText>
                       </div>
                     </div>
@@ -100,7 +100,7 @@ function UserProfile() {
                       <p className="lead fw-normal mb-1">Functionalities</p>
                         <div className="d-flex flex-column align-items-center" style={{ backgroundColor: '#f8f9fa' }}>
                           <MDBBtn className="d-block mb-3" style={{ backgroundColor: 'green' }}>Add Land</MDBBtn>
-                          <MDBBtn className="d-block" style={{ backgroundColor: 'green' }}>Get suggestion</MDBBtn>
+                          <MDBBtn className="d-block" style={{ backgroundColor: 'green' }}>Get analysis</MDBBtn>
                       </div>
                     </div>
                     <div className="d-flex justify-content-between align-items-center mb-4">
