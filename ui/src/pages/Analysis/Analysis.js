64a9b4c0aadc671ad1af52380d695dae231f826b
Alecs21 <ciolacualex32@yahoo.com>
2024-08-11 13:12:30 +0300

    added functionality for buttons under user profile and made the frontend page for analysis

--- ui/src/pages/Analysis/Analysis.js
+++ ui/src/pages/Analysis/Analysis.js
@@ -1,19 +1,38 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import { Helmet } from 'react-helmet'; // Import Helmet
+import { Helmet } from 'react-helmet';
 
-const FormComponent = () => {
+const Analysis = () => {
     const [parameters, setParameters] = useState([]);
     const [startDate, setStartDate] = useState('');
     const [endDate, setEndDate] = useState('');
+    const [selectedLand, setSelectedLand] = useState('');
+    const [lands, setLands] = useState([]);
+    const [error, setError] = useState('');
     const navigate = useNavigate();
 
     useEffect(() => {
         const token = localStorage.getItem('token');
         if (!token) {
             navigate('/login');
+            return;
         }
+
+        const fetchLands = async () => {
+            try {
+                const response = await axios.get('http://127.0.0.1:5000/api/v1/user_lands', {
+                    headers: {
+                        'Authorization': `Bearer ${token}`,
+                    },
+                });
+                setLands(response.data);
+            } catch (error) {
+                console.error('There was an error fetching the lands!', error);
+            }
+        };
+
+        fetchLands();
     }, [navigate]);
 
     const handleCheckboxChange = (event) => {
@@ -27,22 +46,44 @@ const FormComponent = () => {
 
     const handleSubmit = async (event) => {
         event.preventDefault();
+        setError('');
+
+        const start = new Date(startDate);
+        const end = new Date(endDate);
+        const timeDiff = end - start;
+        const dayDiff = timeDiff / (1000 * 3600 * 24);
+
+        if (dayDiff > 7) {
+            setError('The date range cannot exceed 7 days.');
+            return;
+        }
+
+        const formatDate = (date) => {
+            const year = date.getFullYear();
+            const month = ('0' + (date.getMonth() + 1)).slice(-2);
+            const day = ('0' + date.getDate()).slice(-2);
+            return { year, month, day };
+        };
+
+        const startFormatted = formatDate(start);
+        const endFormatted = formatDate(end);
 
         const data = {
             parameters: parameters,
-            start_date: startDate,
-            end_date: endDate,
+            start_date: `${startFormatted.year}-${startFormatted.month}-${startFormatted.day}`,
+            end_date: `${endFormatted.year}-${endFormatted.month}-${endFormatted.day}`,
+            land_id: selectedLand
         };
 
         try {
             const token = localStorage.getItem('token');
-            const response = await axios.post('http://127.0.0.1:5000/analysis', data, {
+            const response = await axios.post('http://127.0.0.1:5000/api/v1/analysis', data, {
                 headers: {
                     'Content-Type': 'application/json',
                     'Authorization': `Bearer ${token}`,
                 },
             });
-            console.log(response.data.message);
+            console.log(response.data);
         } catch (error) {
             console.error('There was an error submitting the form!', error);
         }
@@ -51,30 +92,56 @@ const FormComponent = () => {
     return (
         <>
             <Helmet>
-                <title>Analysis Form - FieldMaster</title> {/* Set the page title */}
+                <title>Analysis Form - FieldMaster</title>
             </Helmet>
-            <form onSubmit={handleSubmit}>
-                <h2>Select Parameters for Analysis</h2>
-                <label>
-                    <input type="checkbox" name="parameters" value="param1" onChange={handleCheckboxChange} /> Parameter 1
-                </label><br />
-                <label>
-                    <input type="checkbox" name="parameters" value="param2" onChange={handleCheckboxChange} /> Parameter 2
-                </label><br />
-                <label>
-                    <input type="checkbox" name="parameters" value="param3" onChange={handleCheckboxChange} /> Parameter 3
-                </label><br />
+            <div style={{ paddingTop: '60px' }}>
+                <form onSubmit={handleSubmit}>
+                    <h2>Select Land</h2>
+                    <select value={selectedLand} onChange={(e) => setSelectedLand(e.target.value)} required>
+                        <option value="">Select a land</option>
+                        {lands.map(land => (
+                            <option key={land.id} value={land.id}>{land.name}</option>
+                        ))}
+                    </select><br />
 
-                <h2>Select Date Range</h2>
-                <label htmlFor="start_date">Start Date:</label>
-                <input type="date" id="start_date" name="start_date" value={startDate} onChange={(e) => setStartDate(e.target.value)} /><br />
-                <label htmlFor="end_date">End Date:</label>
-                <input type="date" id="end_date" name="end_date" value={endDate} onChange={(e) => setEndDate(e.target.value)} /><br />
+                    <h2>Select Parameters for Analysis</h2>
+                    <label>
+                        <input type="checkbox" name="parameters" value="2m_temperature" onChange={handleCheckboxChange} /> 2m Temperature
+                    </label><br />
+                    <label>
+                        <input type="checkbox" name="parameters" value="total_precipitation" onChange={handleCheckboxChange} /> Total Precipitation
+                    </label><br />
+                    <label>
+                        <input type="checkbox" name="parameters" value="volumetric_soil_water_layer_1" onChange={handleCheckboxChange} /> Volumetric Soil Water Layer 1
+                    </label><br />
+                    <label>
+                        <input type="checkbox" name="parameters" value="surface_solar_radiation_downwards" onChange={handleCheckboxChange} /> Surface Solar Radiation Downwards
+                    </label><br />
+                    <label>
+                        <input type="checkbox" name="parameters" value="2m_dewpoint_temperature" onChange={handleCheckboxChange} /> 2m Dewpoint Temperature
+                    </label><br />
+                    <label>
+                        <input type="checkbox" name="parameters" value="10m_u_component_of_wind" onChange={handleCheckboxChange} /> 10m U Component of Wind
+                    </label><br />
+                    <label>
+                        <input type="checkbox" name="parameters" value="10m_v_component_of_wind" onChange={handleCheckboxChange} /> 10m V Component of Wind
+                    </label><br />
+                    <label>
+                        <input type="checkbox" name="parameters" value="soil_temperature_level_1" onChange={handleCheckboxChange} /> Soil Temperature Level 1
+                    </label><br />
 
-                <input type="submit" value="Submit" />
-            </form>
+                    <h2>Select Date Range</h2>
+                    <label htmlFor="start_date">Start Date:</label>
+                    <input type="date" id="start_date" name="start_date" value={startDate} onChange={(e) => setStartDate(e.target.value)} /><br />
+                    <label htmlFor="end_date">End Date:</label>
+                    <input type="date" id="end_date" name="end_date" value={endDate} onChange={(e) => setEndDate(e.target.value)} /><br />
+
+                    {error && <p style={{ color: 'red' }}>{error}</p>}
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
         </>
     );
 };
 
-export default FormComponent;
+export default Analysis;
--- website/views.py
+++ website/views.py
@@ -5,13 +5,12 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 
 from .models import User, Land
 from website import db
-from website import db
 
 import folium
 
 views = Blueprint("views", __name__)
 
-points = [] # dc l am initializat pe asta aici? 
+points = []  # dc l am initializat pe asta aici? 
 
 # @views.route('/api/v1/user_profile', methods=['GET'])
 # @jwt_required()
@@ -38,9 +37,36 @@ def get_user_profile():
         "username": user.username,
         "email": user.email,
         "number_of_lands": len(user.lands),
-        "lands" : [{'id': land.id, 'name':land.name, 'size': land.get_area_surface()} for land in user.lands]  
+        "lands": [{'id': land.id, 'name': land.name, 'size': land.get_area_surface()} for land in user.lands]  
     }
     return jsonify(user_profile), 200
+
+
+@views.route('/api/v1/update_username', methods=['POST'])
+@jwt_required()
+def update_username():
+    current_user_email = get_jwt_identity()
+    user = User.query.filter_by(email=current_user_email).first()
+    
+    if not user:
+        return jsonify({"error": "User not found"}), 404
+
+    data = request.get_json()
+    new_username = data.get('username')
+
+    if not new_username:
+        return jsonify({'error': 'Username cannot be empty'}), 400
+
+    # Verifică dacă username-ul este deja folosit de un alt utilizator
+    if User.query.filter_by(username=new_username).first():
+        return jsonify({'error': 'Username already taken'}), 400
+
+    # Actualizează username-ul utilizatorului curent
+    user.username = new_username
+    db.session.commit()
+
+    return jsonify({'message': 'Username updated successfully', 'username': new_username}), 200
+
 
 @views.route("/api/v1/add_land", methods=["POST"])
 @jwt_required()
@@ -58,7 +84,7 @@ def add_land():
     
     print(points)
     print(name)
-    new_land = Land(name = name, user_id=current_user.id,
+    new_land = Land(name=name, user_id=current_user.id,
                     x1=points[0][0],
                                  y1=points[0][1],
                                  x2=points[1][0],
@@ -77,7 +103,6 @@ def add_land():
     db.session.add(new_land)
     db.session.commit()
     return jsonify({'message': 'Land added succesfully'}), 200
-
 
 
 @views.route("api/v1/get_coordinates", methods=["POST"])
@@ -102,21 +127,71 @@ def get_coordinates() -> dict:
 
     return jsonify(response)
 
-@views.route("/api/v1/analysis", methods=["GET", "POST"])
+@views.route('/api/v1/user_lands', methods=['GET'])
 @jwt_required()
-def analysis() -> str:
-    data = request.get_json()
-    coords = data.get("points")
+def get_user_lands():
+    current_user_jwt = get_jwt_identity()
+    current_user = User.query.filter_by(email=current_user_jwt).first()
+    if current_user is None:
+        return jsonify({"message": "User not found"}), 404
+    
+    lands = [{'id': land.id, 'name': land.name} for land in current_user.lands]
+    return jsonify(lands), 200
+
+
+# @views.route("/api/v1/analysis", methods=["GET", "POST"])
+# @jwt_required()
+# def analysis() -> str:
+#     data = request.get_json()
+#     coords = data.get("points")
+#     current_user = get_jwt_identity()
+#     user = User.query.filter_by(email=current_user).first()
+#     land = Land(user.id, coords)
+
+#     parameters = request.form.getlist("parameters")
+#     start_date = request.form.get("start_date")
+#     end_date = request.form.get("end_date")
+
+#     dict_prompt = { "user": f"{user.id}",
+#                    "parameters": [],
+#                    "year": "",
+#                    "month": [''],
+#                    "day": [''],
+#                    "area": land.get_limits()
+#                    }
+
+#     prompt = f"UserID: {user.id}; LandID: {land.id}; Parameters: {', '.join(parameters)}; Start Date: {start_date}; End Date: {end_date}"
+#     return dict_prompt
+
+@views.route('/api/v1/analysis', methods=['POST'])
+@jwt_required()
+def analysis():
+    data = request.json
+    params = data.get("parameters")
+    start_date = data.get("start_date")
+    end_date = data.get("end_date")
+    land_id = data.get("land_id")
     current_user = get_jwt_identity()
     user = User.query.filter_by(email=current_user).first()
-    land = Land(user.id, coords)
 
-    parameters = request.form.getlist("parameters")
-    start_date = request.form.get("start_date")
-    end_date = request.form.get("end_date")
+    if not user:
+        return jsonify({"message": "User not found"}), 404
 
-    prompt = f"UserID: {user.id}; LandID: {land.id}; Parameters: {', '.join(parameters)}; Start Date: {start_date}; End Date: {end_date}"
-    return prompt
+    land = Land.query.filter_by(id=land_id, user_id=user.id).first()
+    if not land:
+        return jsonify({"message": "Land not found"}), 404
+
+    dict_prompt = {
+        "user": f"{user.id}",
+        "land_id": f"{land.id}",
+        "parameters": params,
+        "start_date": start_date,
+        "end_date": end_date,
+        "area": land.get_limits()
+    }
+
+    # Process the analysis here and return the results
+    return jsonify(dict_prompt), 200
 
 
 # @views.route("/add_land")
